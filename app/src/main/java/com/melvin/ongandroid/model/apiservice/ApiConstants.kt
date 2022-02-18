779f82932fd138b396f41b228fb33144c7daa47b
Jose Luis Mora <josemora9706@gmail.com>
2022-02-18 11:05:28 -0500

    [UPDATE]Se crea el paquete apiservice, ademas se crea el objeto ApiConstants

---
+++ app/src/main/java/com/melvin/ongandroid/model/apiservice/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.melvin.ongandroid.model.apiservice
+
+/**
+ * Este objeto representa las constantes de la API con sus respectivos valores
+ *
+ * @author Jose Luis Mora
+ */
+
+object ApiConstants {
+
+    const val BASE_URL = "http://ongapi.alkemy.org/"
+
+}
