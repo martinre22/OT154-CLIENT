7ba9e8c0605bb8d7702b2c47c1b18299e61e716a
jounob <obeiwiljephnie@gmail.com>
2022-02-19 05:02:20 -0300

    DrawerLayout setup

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -2,11 +2,37 @@ package com.melvin.ongandroid.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appToolbar.toolbar)
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navFragmentHome, R.id.navFragmentAboutUs, R.id.navFragmentActivities,
+            R.id.navFragmentNews, R.id.navFragmentReviews, R.id.navFragmentDonate,
+            R.id.navFragmentContactUs), binding.dlSideBar)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment),
+            appBarConfiguration)
+        binding.navView.setupWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
