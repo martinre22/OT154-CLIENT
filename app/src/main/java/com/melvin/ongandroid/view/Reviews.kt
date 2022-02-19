3b76ac7a019d5c62bfb66da643780daf12245875
jounob <obeiwiljephnie@gmail.com>
2022-02-19 05:07:00 -0300

    Implementation of navigation and fragments

---
+++ app/src/main/java/com/melvin/ongandroid/view/Reviews.kt
@@ -0,0 +1,19 @@
+package com.melvin.ongandroid.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.melvin.ongandroid.R
+
+class Reviews : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reviews, container, false)
+    }
+}
