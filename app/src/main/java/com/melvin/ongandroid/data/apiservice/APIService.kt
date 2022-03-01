69152c16a889c571bfbeceaff84e24da4198ea21
leandriel <83842981+leandriel@users.noreply.github.com>
2022-03-01 18:45:04 -0300

    Leandro valderas/timer splash (#20)
    * connecting api testimonials
    
    * implement splash design
    
    * Timer added to Splash
    
    * Timer added to Splash
    
    * all conflict fixeds

--- app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
+++ app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
@@ -1,10 +1,19 @@
 package com.melvin.ongandroid.data.apiservice
 
 import com.melvin.ongandroid.data.response.ResponseApi
+import com.melvin.ongandroid.model.Contact
 import com.melvin.ongandroid.model.Testimonial
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface APIService {
+    //Tomamos informacion desde testimonios en la API - Leandro Valderas
     @GET( "api/testimonials")
     suspend fun getTestimonials(): ResponseApi<MutableList<Testimonial>>
+
+    @POST("api/contacts")
+    suspend fun postContacts(@Body contactData: Contact): Call<Contact>
+
 }
---
+++ app/src/main/java/com/melvin/ongandroid/model/ContactResponseApi.kt
@@ -0,0 +1,24 @@
+package com.melvin.ongandroid.model
+
+import com.melvin.ongandroid.data.apiservice.APIService
+import com.melvin.ongandroid.data.apiservice.Retrofit2
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ContactResponseApi {
+//    fun postContacts(contactData: Contact, onResult: (Contact?) -> Unit){
+//        val retrofit = Retrofit2.getRetrofit(APIService::class.java)
+//        retrofit.postContacs(contactData).enqueue(
+//            object : Callback<Contact> {
+//                override fun onFailure(call: Call<Contact>, t: Throwable) {
+//                    onResult(null)
+//                }
+//                override fun onResponse(call: Call<Contact>, response: Response<Contact>) {
+//                    val addedPost = response.body()
+//                    onResult(addedPost)
+//                }
+//            }
+//        )
+//    }
+}
