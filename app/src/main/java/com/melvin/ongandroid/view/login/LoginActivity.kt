b44e869c616c8c90016f3811754cd8276cc8e869
Leandro Emanuel Alvarez <leanalvarez.dev@gmail.com>
2022-03-07 18:17:13 -0300

    Revert "Creación de un spinner para indicar que la aplicación se encuentra cargando información (#43)" (#44)
    This reverts commit 1d685ed629828c9a2980150746a3804e6b778944.

--- app/src/main/java/com/melvin/ongandroid/view/login/LoginActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/login/LoginActivity.kt
@@ -1,29 +1,17 @@
 package com.melvin.ongandroid.view.login
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.LogInBinding
-import com.melvin.ongandroid.view.ProgressActivity
-import com.melvin.ongandroid.view.UserRegisterView.SignUpUserViewModel
-import com.melvin.ongandroid.viewmodel.LoginViewModel
 
-class LoginActivity : ProgressActivity() {
-    private val viewModel by viewModels<LoginViewModel>()
+class LoginActivity : AppCompatActivity() {
     private lateinit var binding: LogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        attachLoadingProgressBar(binding.mainView)
-        setUpObservers()
-    }
-
-    private fun setUpObservers() {
-        viewModel.progressBarStatus.observe(this) {
-            setCustomProgressBarVisibility(it)
-        }
     }
 
     //Funcion para mostrar el modal dialog al llamar al endpoint "api/login"
