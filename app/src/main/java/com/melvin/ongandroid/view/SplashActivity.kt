4283c943114c83f59b476f9f12b54bfc7e9995df
Jose Luis Mora <91707234+Obviustick53@users.noreply.github.com>
2022-03-08 15:35:17 -0500

    Jose luis / Navegacion al SignUp desde Login (#46)
    * [UPDATE]Se agrega la navegacion con el boton de signup en el LoginActivity
    
    * [UPDATE]Se agrega la navegacion con el boton de login en el LoginActivity

--- app/src/main/java/com/melvin/ongandroid/view/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.melvin.ongandroid.view.login.LoginActivity
 import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
@@ -13,12 +14,14 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
        // Hacemos que se detenga durante 5 segundos en esta activity - Autor: Leandro Valderas
         activityScope.launch {
             delay(5000)
-            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
+
         }
     }
     //Agregamos el Toast al una vez finalice el Splash - Autor: Leandro Valderas
--- app/src/main/java/com/melvin/ongandroid/view/login/LoginActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/login/LoginActivity.kt
@@ -1,17 +1,67 @@
 package com.melvin.ongandroid.view.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.data.apiservice.Retrofit2
+import com.melvin.ongandroid.data.login.LoginApiService
+import com.melvin.ongandroid.data.login.ResourceLogin
+import com.melvin.ongandroid.data.login.preferences.LoginUserPreferences
+import com.melvin.ongandroid.data.login.repository.LoginRepository
 import com.melvin.ongandroid.databinding.LogInBinding
+import com.melvin.ongandroid.view.MainActivity
+import com.melvin.ongandroid.view.signup_user.SignUpUserActivity
+import com.melvin.ongandroid.viewmodel.login.LoginViewModel
+import com.melvin.ongandroid.viewmodel.login.base.LoginViewModelFactory
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<LoginViewModel>{LoginViewModelFactory(
+        LoginRepository(
+            Retrofit2.buildApi(LoginApiService::class.java))
+    )}
+
     private lateinit var binding: LogInBinding
+    private lateinit var loginUserPreferences: LoginUserPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loginUserPreferences = LoginUserPreferences(this)
+
+        binding.btnSignUp.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignUpUserActivity::class.java))
+        }
+
+        viewModel.loginResponse.observe(this, {
+            when(it){
+                is ResourceLogin.Success -> {
+
+                    lifecycleScope.launch {
+                        loginUserPreferences.saveTokenUser(it.value.data.token)
+                    }
+
+                    Toast.makeText(this ,it.value.message, Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                is ResourceLogin.Failure -> { Toast.makeText(this, "Error al cargar. ", Toast.LENGTH_LONG).show()}
+            }
+        })
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            Log.d("VALIDATION", "EMAIL: ${email}, PASSWORD: ${password}")
+            viewModel.login(email, password)
+        }
     }
 
     //Funcion para mostrar el modal dialog al llamar al endpoint "api/login"
