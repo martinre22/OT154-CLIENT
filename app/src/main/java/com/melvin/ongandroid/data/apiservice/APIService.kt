893f97620c7b06b5b1a45b4a882028e1667d1ece
Martin Re <35383620+martinre22@users.noreply.github.com>
2022-03-10 10:44:35 -0300

    refactor integracion api/slides

--- app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
+++ app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
@@ -3,8 +3,11 @@ package com.melvin.ongandroid.data.apiservice
 import com.melvin.ongandroid.data.response.ResponseApi
 import com.melvin.ongandroid.model.Contact
 import com.melvin.ongandroid.model.Testimonial
+import com.melvin.ongandroid.model.apimodel.SlideModelResponse
 import com.melvin.ongandroid.model.apiservice.apimodel.NewUserResponse
+import com.melvin.ongandroid.model.apiservice.apimodel.NewsResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 interface APIService {
@@ -23,4 +26,13 @@ interface APIService {
         @Field("name") name: String,
         @Field("password") password: String
     ): Call<NewUserResponse>
+
+    //obtenemos lista de slides desde api - Martin Re
+    @GET("api/slides")
+    suspend fun getAllSlides(): Response<SlideModelResponse>
+
+    //obtenemos lista de novedades desde api - Martin Re
+    @GET("api/news")
+    suspend fun getAllNews(): Response<NewsResponse>
+
 }
