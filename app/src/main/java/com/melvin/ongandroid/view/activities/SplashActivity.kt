f8118b4f7dd3acc216a7fba0f81c1a28b16b5c5e
Martin Re <35383620+martinre22@users.noreply.github.com>
2022-03-24 14:25:28 -0300

    Verificar si usuario esta logueado

--- app/src/main/java/com/melvin/ongandroid/view/activities/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/activities/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
+import com.melvin.ongandroid.presentation.splashactivity.SplashViewModel
+import com.melvin.ongandroid.view.MainActivity
 import com.melvin.ongandroid.view.activities.login.LoginActivity
 import kotlinx.coroutines.*
 
@@ -11,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
 
     //Utilizamos Kotlin Coroutine Autor: Leandro Valderas
     val activityScope = CoroutineScope(Dispatchers.Main)
+    private val viewModel by viewModels<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,7 @@ class SplashActivity : AppCompatActivity() {
        // Hacemos que se detenga durante 5 segundos en esta activity - Autor: Leandro Valderas
         activityScope.launch {
             delay(5000)
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
-            startActivity(intent)
+            setObservers()
             finish()
 
         }
@@ -29,5 +32,22 @@ class SplashActivity : AppCompatActivity() {
         activityScope.cancel()
         super.finish()
         Toast.makeText(this, "Timer has finish", Toast.LENGTH_LONG).show()
+    }
+
+    //navegamos a un activity de destino en base si hay un token de usuario almacenado
+    //Martin Re
+    private fun setObservers(){
+        viewModel.userIsLogged.observe(this,{
+            if (it == true){
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }else{
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.setSharedPreferences(this)
     }
 }
