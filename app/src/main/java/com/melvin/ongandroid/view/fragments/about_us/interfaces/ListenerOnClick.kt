1d4230986a9952f7d1c297c7fdec5c15429fee59
Martin Re <35383620+martinre22@users.noreply.github.com>
2022-03-24 08:50:36 -0300

    Martin re-Diseño Vista Detalle Miembro (#63)
    * resolucion errores slider
    
    * up 26-02-22
    
    * up28feb22
    
    * sin cambios
    
    * Navegacion vista detalle Miembro
    
    * DiseñoVistaDetalleMiembro
    
    * DiseñoVistaDetalleMiembro

---
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/about_us/interfaces/ListenerOnClick.kt
@@ -0,0 +1,8 @@
+package com.melvin.ongandroid.view.fragments.about_us.interfaces
+
+import com.melvin.ongandroid.data.local.model.MembersModel
+
+
+interface ListenerOnClick {
+    fun navigateToMemberDetailsFragment(member: MembersModel)
+}
