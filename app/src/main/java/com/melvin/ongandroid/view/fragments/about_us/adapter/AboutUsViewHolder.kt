839e2c133278fdf0aa9ac144f619a9abfb9478c6
Martin Re <35383620+martinre22@users.noreply.github.com>
2022-03-29 10:40:15 -0300

    FixErrorVistaNosotros/VistaMembersDetails

--- app/src/main/java/com/melvin/ongandroid/view/fragments/about_us/adapter/AboutUsViewHolder.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/about_us/adapter/AboutUsViewHolder.kt
@@ -5,35 +5,31 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.melvin.ongandroid.data.local.model.MembersModel
 import com.melvin.ongandroid.databinding.ItemAboutUsBinding
+import com.melvin.ongandroid.view.fragments.about_us.interfaces.ListenerOnClick
 
 
+class AboutUsViewHolder(view: View,
+                        private val click: ListenerOnClick,
+                        private var membersList: List<MembersModel>)
+    : RecyclerView.ViewHolder(view), View.OnClickListener{
 
-class AboutUsViewHolder(view: View,private val aboutUsListener: AboutUsListener): RecyclerView.ViewHolder(view){
     private val binding = ItemAboutUsBinding.bind(view)
 
-//    init {
-//        itemView.setOnClickListener (this)
-//    }
+    init {
+        itemView.setOnClickListener (this)
+    }
 
     fun render(members: MembersModel){
         binding.nameMembers.text = members.name
         binding.positionMembers.text = members.description
         Glide.with(binding.imgAboutUs.context).load(members.image).into(binding.imgAboutUs)
-        binding.facebookUrl.text = members.facebookUrl
-        binding.linkedinUrl.text = members.linkedinUrl
-        binding.facebookUrl.setOnClickListener(){
-            aboutUsListener.facebookLink(members.facebookUrl.toString())
-        }
-        binding.linkedinUrl.setOnClickListener(){
-            aboutUsListener.linkedinLink(members.linkedinUrl.toString())
-        }
 
     }
 
-//    override fun onClick(p0: View?) {
-//        val currentMember = membersList[adapterPosition]
-//        click.navigateToMemberDetailsFragment(currentMember)
-//    }
+    override fun onClick(p0: View?) {
+        val currentMember = membersList[adapterPosition]
+        click.navigateToMemberDetailsFragment(currentMember)
+    }
 
 
 }
