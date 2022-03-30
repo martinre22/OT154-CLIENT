112b5e4ccb45359e4e0e49b407f3ef41aa1cbfee
Martin Re <35383620+martinre22@users.noreply.github.com>
2022-03-29 21:28:58 -0300

    Fix fragment home/fix fragment contactUs (#72)
    * resolucion errores slider
    
    * up 26-02-22
    
    * up28feb22
    
    * sin cambios
    
    * FixFragmentHome/ContactUs

--- app/src/main/java/com/melvin/ongandroid/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/presentation/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.data.repository.GetActivitiesInteractor
 import com.melvin.ongandroid.data.local.model.ActivityModel
+import com.melvin.ongandroid.data.local.model.NewsModel
+import com.melvin.ongandroid.data.repository.GetNewsInteractor
 import kotlinx.coroutines.launch
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
@@ -13,6 +15,7 @@ import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 class HomeViewModel : ViewModel() {
 
     private var getActivitiesInteractor = GetActivitiesInteractor()
+    private var getNewsInteractor = GetNewsInteractor()
 
     //variables livedata para cargar los datos en el carrousel de bienvenida
     //martin re
@@ -24,6 +27,8 @@ class HomeViewModel : ViewModel() {
     private val _carouselIsGone: MutableLiveData<Boolean> = MutableLiveData()
     val carouselIsgone: MutableLiveData<Boolean> = _carouselIsGone
 
+    private val _news: MutableLiveData<List<CarouselItem>> = MutableLiveData()
+    val news : MutableLiveData<List<CarouselItem>> = _news
 
     //metodo de creacion viewmodel, iniciar los metodos privados de viewmodel actual
     //martin re
@@ -48,6 +53,22 @@ class HomeViewModel : ViewModel() {
         }
     }
 
+    fun getListNews(){
+        viewModelScope.launch {
+            val newsInteractor: List<NewsModel> = getNewsInteractor()
+            var carouselList = mutableListOf<CarouselItem>()
+            if (newsInteractor.isNotEmpty()){
+                for (new in newsInteractor){
+                    carouselList.add(CarouselItem(new.image,
+                        new.name.uppercase()))
+                }
+                _news.postValue(carouselList)
+            }else{
+                _carouselIsGone.postValue(true)
+            }
+        }
+    }
+
     /*Agregado Provisional de items, para novedades, creado por Jose Luis Mora.
     Mauro Peña.***
      */
@@ -62,5 +83,9 @@ class HomeViewModel : ViewModel() {
         items.add(CarouselItem("https://th.bing.com/th/id/OIP.gVwjgA2MCF8t4RExmprOHwHaEG?w=325&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"))
 
         return items
+    }
+
+    fun getNews(){
+
     }
 }
